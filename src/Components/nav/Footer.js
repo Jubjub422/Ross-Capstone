5a75b0eed3c8c1ea22564f574f9cfd51310f09fa
Jubjub422 <ross.stovenour@gmail.com>
2021-12-17 16:03:13 -0600

    small updates, styling

--- src/Components/nav/Footer.js
+++ src/Components/nav/Footer.js
@@ -1,26 +1,25 @@
+import { useHistory } from "react-router-dom"
 import "./Footer.css"
 
 
 
-
-
 export const Footer =() => {
-
+const history = useHistory()
 
 
 
 
     return ( <>
         <section className="footer">
-         <div>
+         <div className="footerDiv">
          <button type="button"
-             className="btn newGrind"
+             className="footerButton newGrind"
              onClick={() => { history.push("/grinds/new") }}>
              Make a new grind
          </button>
      
-         <button type="button addToGrind"
-             className="btn"
+         <button type="button"
+             className="footerButton addToGrind"
              onClick={() => { history.push("/tasks") }}>
              Want to add tasks to existing grinds?
          </button>
